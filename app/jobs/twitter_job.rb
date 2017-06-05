549ea549693341e8e60ff09c876aada8cd64b433
Roo <rosiebrigham@gmail.com>
2017-06-05 13:54:37 +0100

    refactors jobs a bit (probs needs some tests tho

--- app/jobs/twitter_job.rb
+++ app/jobs/twitter_job.rb
@@ -14,43 +14,43 @@ end
 
 class TwitterJob
   def perform
-
     client.search("#upcyclegarden").each do |tweet|
       tweet.media.each do |media|
-        if !Image.find_by(twitter_id: media.id)
-          twitter_desc = tweet.text
-          image_id = media.id
-          url = URI.parse(media.media_url).to_s
-          user = tweet.user
-
-
-          twitter_user = TwitterUser.find_by(twitter_id: user.id)
-
-          if !twitter_user
-            user_id = user.id
-            user_name = user.screen_name
-            user.geo_enabled? ? false : location = user.location
-            friends_count = user.friends_count
-            followers_count = user.followers_count
-
-            twitter_user = TwitterUser.new(
-                                    twitter_id: user_id,
-                                    user_name: user_name,
-                                    location: location,
-                                    followers_count: followers_count,
-                                    friends_count: friends_count
-             )
-             twitter_user.save
-           end
-
-           image = Image.new(url: url, twitter_id: image_id, site: 'twitter', twitter_user_id: twitter_user.id)
-           image.save
-          end
-        end
+        Image.find_by(twitter_id: media.id) ? "" : create_image(tweet, media)
       end
     end
+  end
 
   private
+
+  def create_image(tweet, media)
+    twitter_desc = tweet.text
+    image_id = media.id
+    url = URI.parse(media.media_url).to_s
+    user = tweet.user
+    twitter_user = TwitterUser.find_by(twitter_id: user.id) || create_twitter_user(user)
+    # require "pry"; binding.pry
+    image = Image.new(url: url, twitter_id: image_id, site: 'twitter', twitter_user_id: twitter_user.id)
+    image.save
+  end
+
+  def create_twitter_user(user)
+    user_id = user.id
+    user_name = user.screen_name
+    location = user.geo_enabled? ? user.location : "not given"
+    friends_count = user.friends_count
+    followers_count = user.followers_count
+
+    twitter_user = TwitterUser.new(
+                            twitter_id: user_id,
+                            user_name: user_name,
+                            location: location,
+                            followers_count: followers_count,
+                            friends_count: friends_count
+     )
+     twitter_user.save
+     twitter_user
+  end
 
   def client
     @client ||= TwitterClient.new.client
