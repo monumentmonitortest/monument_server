093eda88043ed349b49ed3a902f983c89b5e5f6a
Roo <rosiebrigham@gmail.com>
2017-06-09 15:30:09 +0100

    Methods on image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,15 @@
 class Image < ApplicationRecord
   validates :url, presence: true,
                     length: { minimum: 5 }
+
+  SITES = ["MACHRIE", "HOLYROOD"]
+  SOURCE = ["TWITTER", "INSTAGRAM", "UPLOAD"]
+
+  def machrie?
+    self.site == "MACHRIE"
+  end
+
+  def holyrood?
+    self.site == "HOLYROOD"
+  end
 end
