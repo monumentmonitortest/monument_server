8cdd34052daf9902367cb440e2f52f792db2d2f1
Roo <rosiebrigham@gmail.com>
2017-07-15 22:24:26 +0100

    gets search working and some caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  get '/images/search', to: 'images#search'
+
   resources :images
-  
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
