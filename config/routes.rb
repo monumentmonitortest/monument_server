1cdddfa495a01ded425d4636aeedba299388bfa8
Roo <rosiebrigham@gmail.com>
2017-07-25 11:31:36 +0100

    Puts in json and css response for d3

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/images/search', to: 'images#search'
   get '/csv', to: 'images#csv', as: :csv
 
+  get '/machrie', to: 'images#machrie'
 
+  get '/d3/machrie', to: 'd3#machrie'
+  
   root 'welcome#index'
 end
