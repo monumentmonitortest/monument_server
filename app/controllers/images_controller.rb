2e33f60e0ddb788423a735e135c0572c62c580ef
Roo <rosiebrigham@gmail.com>
2017-08-07 18:18:26 +0100

    changes params again coz why not

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -71,7 +71,7 @@ class ImagesController < ApplicationController
   end
 
   def image_params
-    params.require(:image).permit(:url, :site, :reliable, :record_taken, :source)
+    params.require(:image).permit(:reliable)
   end
 
 end
