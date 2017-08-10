43c25e773e3773796bbee13557fee25f8bcf3175
Roo <rosiebrigham@gmail.com>
2017-08-10 18:22:04 +0100

    changes permitted params again

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -71,7 +71,7 @@ class ImagesController < ApplicationController
   end
 
   def image_params
-    params.require(:image).permit(:reliable, :site)
+    params.require(:image).permit(:reliable, :source, :site)
   end
 
 end
