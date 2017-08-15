40bd2747ef0be44f028a2b3d75d0bac544776f33
Roo <rosiebrigham@gmail.com>
2017-08-15 09:38:34 +0100

    adds rainfall specifically to CSV

--- app/models/image.rb
+++ app/models/image.rb
@@ -26,7 +26,7 @@ class Image < ApplicationRecord
   end
 
   def self.set_weather_attribute(image, attr)
-    !image.send(attr).nil? ? image.send(attr)["data"][0] : image.send(attr)
+    !image.send(attr).nil? ? image.send(attr)["data"][0]['precipIntensityMax'] : image.send(attr)
   end
 
   def self.set_record_taken_attribute(image, attr)
