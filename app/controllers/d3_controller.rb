805a97bd2f7cde965d1c4f873458ac842ac36c58
Rosie-Brigham <rosiebrigham@gmail.comm>
2018-10-22 15:51:15 +0100

    Removes defunked d3 code

--- app/controllers/d3_controller.rb
+++ app/controllers/d3_controller.rb
@@ -1,10 +1,4 @@
 class D3Controller < ApplicationController
-
-  def machrie
-    respond_to do |format|
-      images = Image.all.where(site: 'MACHRIE').to_json
-      format.json  { render :json => images } # don't do msg.to_json
-      format.csv { send_data images, filename: "machrie-#{Date.today}.csv" }
-    end
-  end
+  # TODO - implement visualisation for different sites
+  # TODO - think what this will look like, and list differentthigns to display
 end
