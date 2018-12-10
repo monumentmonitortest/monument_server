d3f21f0b78cd96e7c7fcee3d9ef85d88f97b5349
Rosie-Brigham <rosiebrigham@gmail.comm>
2018-12-10 12:18:44 +0000

    Adds basic auth

---
+++ app/controllers/concerns/http_auth_concern.rb
@@ -0,0 +1,13 @@
+module HttpAuthConcern  
+  extend ActiveSupport::Concern
+  included do
+    before_action :http_authenticate
+  end
+  
+  def http_authenticate
+    return true unless Rails.env == 'production'
+    authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['AUTH_USERNAME'] && password == ENV['AUTH_PASSWORD']
+    end
+  end
+end
