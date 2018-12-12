cca0724c348d01a68de1d633d95dc2c50946193d
Rosie-Brigham <rosiebrigham@gmail.comm>
2018-12-12 14:09:30 +0000

    Getting those views and actions there, using smart_listing

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,16 +1,29 @@
 class SubmissionsController < ApplicationController
   include SmartListing::Helper::ControllerExtensions
   helper  SmartListing::Helper
+  before_action :set_submission, only: [:show, :edit, :update, :destroy]
 
   def index
-    @submissions = smart_listing_create(:submissions, Submission.all, partial: "submissions/listing")
+    @submissions = smart_listing_create(:submissions, Submission.all, partial: "submissions/list")
   end
 
   def show
     @submission = Submission.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    binding.pry
+    @submission.update_attributes(submission_params)
+  end
+
   private
+  def set_submission
+    @submission = Submission.find(params[:id])
+  end
+
   def submission_params
     params.require(:submission).permit(:reliable, :site_id, :type_id, :image, :record_taken, :tags)
   end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,7 +1,11 @@
 class Submission < ApplicationRecord
   belongs_to :site
   belongs_to :type
+  
+  validates_presence_of :type_id
+  # validates site_id is an actual site
 
+  
   # when they are scraped, it will be 'unclassified' (no site)
   # TODO: should maybe have validation on type
 end
