a844a9124c2e1786cd9d6f136776de64afbd8f75
Rosie-Brigham <rosiebrigham@gmail.comm>
2019-04-18 17:07:09 +0100

    adds validations on type

--- app/models/type.rb
+++ app/models/type.rb
@@ -2,7 +2,8 @@ class Type < ApplicationRecord
   belongs_to :submission
   # delete data - it's shit
   store_accessor :data, :email_address, :number, :insta_username, :twitter_username 
-  
+  validate :validate_params, on: :create
+
   before_create :annonymize_present_data
 
   NAMES = %w(INSTAGRAM EMAIL TWITTER WHATSAPP OTHER )
@@ -21,5 +22,27 @@ class Type < ApplicationRecord
     Digest::SHA1.hexdigest data
   end  
 
-  # validate presence of the right things here...
+  def validate_params
+    validate_email if name == "EMAIL" 
+    validate_instagram if name == "INSTAGRAM" 
+    validate_whatsapp if name == "WHATSAPP" 
+    validate_twitter if name == "TWITTER" 
+  end
+
+  def validate_email
+    errors.add(:type, "Email not present") unless email_address.present? 
+  end
+
+  def validate_instagram
+    errors.add(:type, "Instagram user not present") unless insta_username.present? 
+  end
+
+  def validate_twitter
+    errors.add(:type, "Twitter handle not present") unless twitter_username.present? 
+  end
+
+  def validate_whatsapp
+    errors.add(:type, "Whatsapp number not present") unless number.present? 
+  end
 end
+
