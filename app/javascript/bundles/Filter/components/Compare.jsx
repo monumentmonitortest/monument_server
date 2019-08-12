e84930033aacf3532d153d1e8f57700701ee0726
Rosie Brigham <rosiebrigham@gmail.com>
2019-08-12 12:57:19 +0100

    selects two images to compare(ish)

--- app/javascript/bundles/Filter/components/Compare.jsx
+++ app/javascript/bundles/Filter/components/Compare.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import ImagePreview from './ImagePreview.jsx'
+
 export default class Compare extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return (
-      <div className="flex flex-wrap justify-around items-center mv3 sticky">
-        <div className="pv4 tc w-25">select image to compare</div>
-        <div className="pv4 tc w-25">Select image to compare</div>
+      <div className="flex flex-wrap justify-around items-center h4 mt0 mb5 sticky blue-background">
+        <div className="pv4 tc w-25"><ImagePreview index="0" imageCompare={this.props.imageCompare} /></div>
+        <div className="pv4 tc w-25"><ImagePreview index="1" imageCompare={this.props.imageCompare} /></div>
         <div className="pv3 tc w-50"><button>Compare images</button></div>
       </div>
     )
