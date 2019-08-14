82e6259258a7925200cd425139a19faa9e65258e
Rosie Brigham <rosiebrigham@gmail.com>
2019-08-14 15:30:11 +0100

    wip spec

--- spec/controllers/csv_controller_spec.rb
+++ spec/controllers/csv_controller_spec.rb
@@ -1,38 +1,32 @@
 require "rails_helper"
 RSpec.describe CsvController do
   
-  context "#results" do
+  xcontext "#create" do
     render_views
     
-    let(:site)            { create(:site) }
-    let(:params)          { {site_id: site.id} }
-    let(:service_double)  { double(:create_csv_service) }
-    let(:result)          { "csv,file" }
-
+    
     login_user
     before do
-      allow(CSVCreateService).to receive(:new).and_return(service_double)
-      allow(service_double).to receive(:create).and_return(result)
     end
     
-    it "calls the CSV create service" do
-      expect(service_double).to receive(:create)
-      post :results,  { params: params, format: :csv }
-      expect(response.body).to eq result
-    end
+    # it "calls the CSV create service" do
+    #   expect(service_double).to receive(:create)
+    #   post :results,  { params: params, format: :csv }
+    #   expect(response.body).to eq result
+    # end
 
-    context "response" do
-      before do
-        post :results,  { params: params, format: :csv }
-      end
+    # context "response" do
+    #   before do
+    #     post :results,  { params: params, format: :csv }
+    #   end
 
-      it "returns a 200" do
-        expect(response.status).to eq(200)
-      end
+    #   it "returns a 200" do
+    #     expect(response.status).to eq(200)
+    #   end
       
-      it "returns a CSV file" do
-        expect(response.content_type).to eq "text/csv"
-      end
+    #   it "returns a CSV file" do
+    #     expect(response.content_type).to eq "text/csv"
+    #   end
     end
   end
 end
