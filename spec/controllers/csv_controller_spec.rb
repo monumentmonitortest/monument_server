09fb449079ce7a9b35b7d1ed1d4e7ed6bc94e5ef
Rosie Brigham <rosiebrigham@gmail.com>
2019-08-15 10:01:04 +0100

    fixes csv controller spec

--- spec/controllers/csv_controller_spec.rb
+++ spec/controllers/csv_controller_spec.rb
@@ -1,32 +1,18 @@
 require "rails_helper"
 RSpec.describe CsvController do
   
-  xcontext "#create" do
+  context "#create" do
     render_views
-    
     
     login_user
     before do
     end
     
-    # it "calls the CSV create service" do
-    #   expect(service_double).to receive(:create)
-    #   post :results,  { params: params, format: :csv }
-    #   expect(response.body).to eq result
-    # end
-
-    # context "response" do
-    #   before do
-    #     post :results,  { params: params, format: :csv }
-    #   end
-
-    #   it "returns a 200" do
-    #     expect(response.status).to eq(200)
-    #   end
-      
-    #   it "returns a CSV file" do
-    #     expect(response.content_type).to eq "text/csv"
-    #   end
+    it "uploads a whole bunch of instagram posts" do
+      # TODO - if this is how it will go
+      # expect(service_double).to receive(:create)
+      # post :results,  { params: params, format: :csv }
+      # expect(response.body).to eq result
     end
   end
 end
