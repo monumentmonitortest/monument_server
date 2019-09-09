9984ecf508e8c84245662630faa90b9232b9b7df
Rosie Brigham <rosiebrigham@gmail.com>
2019-09-09 10:53:46 +0100

    changes csv controller to accept any file

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -1,46 +1,15 @@
 class CsvController < ApplicationController
   before_action :redirect_unless_admin
-
+  # TODO - add date in
   def index
   end
 
   def create
+    date = params["from_date"]
     file = params["CSV-file"].read
     data = JSON.parse(file)
-    import_instagram(data)
+    
+    InstaBacklogJob.new(data, date).perform
     redirect_to '/admin'
-  end
-
-  private
-  
-  def import_instagram(data)
-    data.each do |submission|
-      Type.find_by(type_specific_id: submission["imageUrl"]) ? "" : create_registration(submission)
-    end
-  end
-
-  def instagram_site_id
-    Site.find_by(name: 'Instagram unsorted').id
-  end
-
-  def create_registration(hash)
-    registration = Registration.new(reliable: false,
-                                    site_id: instagram_site_id,
-                                    image_file: hash["imageUrl"],
-                                    record_taken: hash["timestamp"],
-                                    type_name: "INSTAGRAM",
-                                    email_address: "",
-                                    number: "",
-                                    insta_username: hash["url"], #this is wrong...
-                                    twitter_username: "",
-                                    type_specific_id: hash["imageUrl"],
-                                    comment: hash["firstComment"])
-
-    if registration.save
-      puts "registration save succesfull"
-    else
-      binding.pry
-      puts "whoops, there's been an error:"
-    end
   end
 end
