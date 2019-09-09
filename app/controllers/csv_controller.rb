fe641ddaf5b2da1c40efecfc05246d935edb761e
Rosie Brigham <rosiebrigham@gmail.com>
2019-09-09 16:18:06 +0100

    sod it

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -9,7 +9,7 @@ class CsvController < ApplicationController
     file = params["CSV-file"].read
     data = JSON.parse(file)
     
-    InstagramUploadJob.perform_later(data, date)
+    InstagramUploadJob.perform(data, date)
     redirect_to '/admin'
   end
 end
