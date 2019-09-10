6cde38717f1d82808e13def775a9bfc656d34c56
Rosie Brigham <rosiebrigham@gmail.com>
2019-09-10 15:55:36 +0100

    creates reports

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,29 @@
+class ResultsController < ApplicationController
+  DATE = '01/09/2018'.to_date
+
+  def index
+    @type_numbers = create_type_numbers_array
+    @site_numbers = create_site_numbers
+  end
+
+  private
+
+  def create_type_numbers_array
+    Type::NAMES.map do |name|
+      Submission.joins(:type).where(types: { name: name }).where("record_taken > ?", DATE).count
+    end
+  end
+
+  def create_site_numbers
+    Site.all.map do |site|
+      {
+        name: site.name, 
+        submissions: site.submissions.count,
+        twitter: site.submissions.joins(:type).where(types: { name: "TWITTER" }).where("record_taken > ?", DATE).count,
+        instagram: site.submissions.joins(:type).where(types: { name: "INSTAGRAM" }).where("record_taken > ?", DATE).count,
+        email: site.submissions.joins(:type).where(types: { name: "EMAIK" }).where("record_taken > ?", DATE).count,
+        whatsapp: site.submissions.joins(:type).where(types: { name: "WHATSAPP" }).where("record_taken > ?", DATE).count
+      }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   
     resources :insta_upload
     
+    resources :results
+
+    # get '/person-specific-type-results', to: 'results#person_specific_type_results'
     # post '/sites/csv/results', to: 'csv#results', as: :results
   end
 
@@ -24,6 +27,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :submissions
+      get :type_specific_report, to: 'csv#type_specific'
+      get :basic_submission_report, to: 'csv#basic_submission'
     end
   end
 end
