2b5cc5107e3467dbc1e401171d582ff3d2cb600b
Rosie Brigham <rosiebrigham@gmail.com>
2019-09-10 15:58:55 +0100

    fixing specs

---
+++ spec/controllers/results_controller_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+RSpec.describe ResultsController do
+  
+  context "#show" do
+    render_views
+    
+    login_user
+    before do
+    end
+    
+    it "shows some results" do
+    end
+  end
+end
