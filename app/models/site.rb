0fbcd89c47585170781f04272d71f67476da7e06
Rosie Brigham <rosiebrigham@gmail.com>
2019-09-11 18:27:25 +0100

    removes unsuitable sites

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,5 @@
 class Site < ApplicationRecord
   has_many :submissions
 
-  UNSORTED_SITES = ['Instagram unsorted', 'Twitter unsorted', 'unidentified'].freeze
+  UNSORTED_SITES = ['Instagram unsorted', 'Twitter unsorted', 'Unsuitable'].freeze
 end
