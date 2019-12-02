46891aa025ed718845b143717e6cff2163ff32c7
Rosie Brigham <rosiebrigham@gmail.com>
2019-12-02 16:42:16 +0000

    putting a pin in work done so far...

--- app/javascript/bundles/Filter/components/DataVis.jsx
+++ app/javascript/bundles/Filter/components/DataVis.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VictoryBar, VictoryChart, VictoryTheme } from 'victory';
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryAxis } from 'victory';
 
 export default class DataVis extends React.Component {
   constructor(props) {
@@ -13,18 +13,26 @@ export default class DataVis extends React.Component {
       <div>
         <h1>Last Month</h1>
         <VictoryChart
-          theme={VictoryTheme.material}
+          // theme={VictoryTheme.grayscale}
           domainPadding={10}
         >
+          <VictoryAxis 
+            style={{tickLabels: {fill: "#bbbbbb"}}} />
           <VictoryBar
-            style={{ data: { fill: "#c43a31" } }}
             data={dailyData}
+            labels={({ datum }) => `${datum.y}`}
             animate={{
               duration: 2000,
               onLoad: { duration: 1000 }
             }}
             barRatio={0.8}
-            // labels={dailyData}
+            alignment="middle"
+            style={{
+              data: { fill: ({ datum }) => datum.y > 0 ? "#bbbbbb" : "#dcdbdb" },
+              labels: { fontSize: ({ text }) => text.length > 10 ? 8 : 12, fill: "#bbbbbb"},
+              parent: { border: "1px solid #ccc" },
+              x: { fill:  "#bbbbbb"}
+            }}
           />
         </VictoryChart>
       </div>
