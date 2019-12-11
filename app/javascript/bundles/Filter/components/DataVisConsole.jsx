a0b4322ef9414ebea1c88af1bb6f8ff8f4f7b5aa
Rosie Brigham <rosiebrigham@gmail.com>
2019-12-11 19:35:43 +0000

    Update DataVisConsole.jsx

--- app/javascript/bundles/Filter/components/DataVisConsole.jsx
+++ app/javascript/bundles/Filter/components/DataVisConsole.jsx
@@ -15,12 +15,46 @@ export default class DataVis extends React.Component {
     const dailyData = this.props.submissionsData != "undefined" ? this.props.submissionsData.byMonth : []
     const typeData = this.props.submissionsData != 'undefined' ? this.props.submissionsData.types : []
     const tagData = this.props.submissionsData != 'undefined' ? this.props.submissionsData.tags : []
-    
+    const maxData = this.props.submissionsData != 'undefined' ? this.props.submissionsData.maxSubs : []
+    const minData = this.props.submissionsData != 'undefined' ? this.props.submissionsData.minSubs : []
+   
     return (
       <div>
+        <div className="pa4 mt4 br2 data-box">
+          <h2 className="mb4 f1 title ">Project wide stats</h2>
+          <div className="flex items-end">
+            <div className="w-30">
+              <h1 className="data-bold-color">{this.props.allSubmissionsTotal}</h1> 
+              <hr className="w-60 ml0"></hr>
+              Submissions to date
+            </div>
+            
+            <div>
+              {maxData.map((site)=>(<h3 className="f2 mb3 data-bold-color">{site}</h3>))}
+              <hr className="w-60 ml0"></hr>
+              Most popular
+            </div>  
+
+            <div>
+              {minData.map((site)=>(<h3 className="f2 mb3 data-bold-color">{site}</h3>))}
+              <hr className="w-60 ml0"></hr>
+              Least popular
+            </div>
+            
+            <div className="w-40">
+              <TypePieChart data={typeData} />
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
         {/* <SubmissionAreaChart data={dailyData} /> */}
         <SubmissionBarChart data={dailyData} />
-        <TypePieChart data={typeData} />
         <TagScatterChart data={tagData} />
       </div>
     )
