68c10f994bc64817a4997b2fca24ad7078199dd5
Rosie Brigham <rosiebrigham@gmail.com>
2019-12-11 19:32:48 +0000

    starts making it look all professional like

--- app/javascript/bundles/Filter/components/data/TypePieChart.jsx
+++ app/javascript/bundles/Filter/components/data/TypePieChart.jsx
@@ -8,16 +8,14 @@ export default class TypePieChart extends React.Component {
   
   render() {
     return (
-      <div className="w-40 pt4">
-          <VictoryPie
-            colorScale={["#00b753", "#38A1F3", "#B23121", "#833AB4"]}
-            data={this.props.data}
-            labelRadius={140}
-            radius={200}
-            innerRadius={130}
-            style={{ labels: { fill: "white", fontSize: 12, fontWeight: "" } }}
-          />
-        </div>
+      <VictoryPie
+        colorScale={["#00b753", "#38A1F3", "#B23121", "#833AB4"]}
+        data={this.props.data}
+        labelRadius={140}
+        radius={200}
+        innerRadius={130}
+        style={{ labels: { fill: "white", fontSize: 12, fontWeight: "" } }}
+      />
     )
   }
 }
