ce8eb6b7f86cbd27c86ff5f4f005c174cc723937
Rosie Brigham <rosiebrigham@gmail.com>
2020-04-17 15:42:14 +0100

    adds 'submitted at' field to submission

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_143639) do
+ActiveRecord::Schema.define(version: 2020_04_17_142825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_02_07_143639) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.jsonb "metadata", default: {}, null: false
+    t.datetime "submitted_at"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
