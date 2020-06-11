33ded93d4314ce8c0fd8f82cbac06e86e00fe8f4
Rosie Brigham <rosiebrigham@gmail.com>
2020-06-11 18:25:38 +0100

    creates a worker, to zip file async

--- spec/workers/image_variant_worker_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-RSpec.describe ImageVariantWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
---
+++ spec/workers/submission_zip_worker_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
+
+RSpec.describe SubmissionZipWorker, type: :worker do
+  let(:site) { create(:site) }
+  
+  describe "#perform" do
+
+    context "when called" do
+      it "creates an async job" do
+        expect {
+          described_class.perform_async(site.id)
+        }.to change(SubmissionZipWorker.jobs, :size).by(1)
+      end
+
+      # Sidekiq::Testing.inline! do
+        it "calls image zip creation service" do
+          service = double("service double")
+          allow(ImageZipCreationService).to receive(:new).and_return(service)
+          allow(service).to receive(:create)
+          
+          expect(service).to receive(:create)
+          described_class.perform_async(site.id)
+          Sidekiq::Worker.drain_all
+        end
+      end
+    # end
+
+    context "given a site id" do
+
+      it "creates a zip file" do
+
+      end
+
+      it "zipfile contains all images from sites submission" do
+
+      end
+    end
+  end
+end
