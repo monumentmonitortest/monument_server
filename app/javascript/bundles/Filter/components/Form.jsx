72441bcbeb9c6c6b54331e1f66782bdda5559bb7
Rosie-Brigham <rosiebrigham@gmail.com>
2020-09-12 12:15:27 +0100

    gets dropdown with multip select in place for tags

--- app/javascript/bundles/Filter/components/Form.jsx
+++ app/javascript/bundles/Filter/components/Form.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MultiSelect from "react-multi-select-component";
 
 export default class Form extends React.Component {
   constructor(props) {
@@ -7,7 +8,8 @@ export default class Form extends React.Component {
     this.state = {
       site: '',
       type: '',
-      tag: '',
+      // tag: '',
+      selected: [],
       reliable: false
     }
   }
@@ -29,12 +31,24 @@ export default class Form extends React.Component {
       [name]: value
     });
   }
+
+  setSelectedTag = event => {
+    // this puts the whole event object in state
+    // so work has to be done in handle submit to reduce get just values
+    const tag = event
+    this.setState({selected: tag})
+  }
   
   render() {
+    const {selected} = this.state
+    const options = this.props.tags.map(tag => { return {label: tag, value: tag}})
+    const selectString = {
+                            selectSomeItems: "Select tags",
+                          }
     return (
       <form className="ph4 pv4 mb0 br1" onSubmit={this.handleSubmit}>
           <div className="flex flex-wrap items-center justify-between">
-            <span className="h-25">
+            <span className="h-25 w-100">
               <select id="site" className="dark-color w-100" onChange={this.handleInputChange}>
                 <option defaultValue="">Select Site</option>
               {this.props.siteNames.sort().map((site, i) => 
@@ -45,7 +59,7 @@ export default class Form extends React.Component {
               </select>
             </span>
 
-            <span className="h-25">
+            <span className="h-25 w-100">
               <input 
                 placeholder="Select submission type"
                 type="text" 
@@ -55,20 +69,17 @@ export default class Form extends React.Component {
                 onChange={this.handleInputChange} />
             </span>
 
-            {/* <span className="h-25"> */}
-              {/* <label>
-                Useful?
-              </label>
-              <input
-                name="useful"
-                id="reliable"
-                type="checkbox"
-                className=''
-                checked={this.state.reliable}
-                onChange={this.handleInputChange} /> */}
-            {/* </span> */}
-
-            <span className="h-25">
+            <span className="h-25 w-100">
+              <MultiSelect
+                options={options}
+                value={selected}
+                onChange={this.setSelectedTag}
+                labelledBy={"Select"}
+                overrideStrings={selectString}
+                />
+            </span>
+              
+            {/* <span className="h-25">
               <select id="tag" className="dark-color w-100" onChange={this.handleInputChange}>
                 <option defaultValue="">Select Tag</option>
                   {this.props.tags.map((tag) => 
@@ -77,7 +88,7 @@ export default class Form extends React.Component {
                       key={tag}
                       className="dark-color w-100">{tag}</option>)}
               </select>
-            </span>
+            </span> */}
 
             <span className="h-25">
               <button className="flex center mt4 white-background dark-color" type="submit" onClick={this.handleSubmit}>
