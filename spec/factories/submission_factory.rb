a336fca71dfbd82dd001ec90926eda7e1597dd8a
Rosie-Brigham <rosiebrigham@gmail.com>
2020-09-14 17:24:59 +0100

    adds participant factory

--- spec/factories/submission_factory.rb
+++ spec/factories/submission_factory.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :submission do
     site_id { create(:site).id}
+    participant_id { create(:participant).id }
     record_taken { Date.today }
     ai_tags  { {} }
     image { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'assets', 'test-image.jpg'), 'image/jpg') }
