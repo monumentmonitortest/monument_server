5acc4e1f55cf9f31fa7e4ebef614cda03996a09f
Rosie-Brigham <rosiebrigham@gmail.com>
2020-09-21 16:17:04 +0100

    gets data view working with tags and type filters

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -14,10 +14,9 @@ module Api
 
       def data
         # TODO - add some specs you lazy sod.
-        date = Date.today - 1.year
-        submissions ||= Submission.where('record_taken >= ?', date).search_site(site_filter)
+        submissions ||= scope_without_images
 
-        submissions_data = SubmissionsDataCreateService.new(submissions, date).create
+        submissions_data = SubmissionsDataCreateService.new(submissions, Date.today - 1.year).create
         render json: submissions_data
       end
 
@@ -28,7 +27,9 @@ module Api
         end
 
         def scope_without_images
-          Submission.search_site(site_filter).type_search(type_filter)
+          date = Date.today - 1.year
+
+          Submission.where('record_taken >= ?', date).search_site(site_filter).type_search(type_filter).with_tags(tag_filter)
         end
 
         def permitted_params
