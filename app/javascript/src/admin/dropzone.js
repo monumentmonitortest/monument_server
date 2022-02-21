0afe5b7331fa19c6e5915a627afef75858ec6023
Rosie-Brigham <rosiebrigham@gmail.com>
2022-02-21 14:24:35 +0000

    only fires dropzone on correct page

--- app/javascript/src/admin/dropzone.js
+++ app/javascript/src/admin/dropzone.js
@@ -1,73 +1,84 @@
 import Dropzone from "dropzone";
 
+if (document.getElementById('bulk-upload')) {
+  readyBulkUpload();
+}
+
+if (document.getElementById('drop-upload')) {
+  readyDropUpload();
+}
 
 // ------------ ********* --------------
 // Dropzone for drop upload
 // ------------ ********* --------------
-const newDropzone = new Dropzone("#simpledropzone", {
-  url: "/admin/drop_upload"
-});
+function readyDropUpload() {
+  const newDropzone = new Dropzone("#simpledropzone", {
+    url: "/admin/drop_upload"
+  });
+}
 
 
 // ------------ ********* --------------
 // Dropzone for old bulk upload
 // ------------ ********* --------------
-const bulkDropzone = new Dropzone("#bulkdropzone", {
-  url: "/admin/bulk_upload",
-  autoProcessQueue: false,
-  autoQueue: true,
-  uploadMultiple: true,
-  parallelUploads: 2,
-  maxFiles: 100,
-  ignoreHiddenFiles: true,
-  // acceptedFiles: '*.jpg',
-  previewsContainer: '#dz-preview-container',
-
-  init: function() {
-    var myDropzone = this;
-    console.log('initialized bulk dropzone')
-    var form = document.getElementById('bulkdropzone');
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-      myDropzone.processQueue();
-    });
-
-    this.on("addedfile", file => {
-      console.log(`File added: ${file.name}`);
-    });
-    this.on('sending', function(file, xhr, formData) {
-      console.log('hey')
-    });
-
-    
-    this.on("sendingmultiple", function(file, xhr, formData) {
-      console.log('send multiple')
-      formData.append("site_id", $('#myDropzoneForm_site_id').val());
+function readyBulkUpload() {
+  const bulkDropzone = new Dropzone("#bulkdropzone", {
+    url: "/admin/bulk_upload",
+    autoProcessQueue: false,
+    autoQueue: true,
+    uploadMultiple: true,
+    parallelUploads: 2,
+    maxFiles: 100,
+    ignoreHiddenFiles: true,
+    // acceptedFiles: '*.jpg',
+    previewsContainer: '#dz-preview-container',
+  
+    init: function() {
+      var myDropzone = this;
+      console.log('initialized bulk dropzone')
+      var form = document.getElementById('bulkdropzone');
+      form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        myDropzone.processQueue();
+      });
+  
+      this.on("addedfile", file => {
+        console.log(`File added: ${file.name}`);
+      });
+      this.on('sending', function(file, xhr, formData) {
+        console.log('hey')
+      });
+  
       
-      var reliable = $('#tpl').find('#reliable').is(':checked')
-      var date = $('#tpl').find('#record_taken').val()
-      var submittedAt = $('#tpl').find('#submitted_at').val()
-      var typeName = $('#tpl').find('#type_name').val()
+      this.on("sendingmultiple", function(file, xhr, formData) {
+        console.log('send multiple')
+        formData.append("site_id", $('#myDropzoneForm_site_id').val());
+        
+        var reliable = $('#tpl').find('#reliable').is(':checked')
+        var date = $('#tpl').find('#record_taken').val()
+        var submittedAt = $('#tpl').find('#submitted_at').val()
+        var typeName = $('#tpl').find('#type_name').val()
+        
+        var participantId = $('#tpl').find('#participant_id').val()
+        var comment = $('#tpl').find('#comment').val()
+        
+        formData.append('reliable', reliable);
+        formData.append('record_taken', date);
+        formData.append('submitted_at', submittedAt);
+        formData.append('type_name', typeName);
+        formData.append('comment', comment);
+        formData.append('participant_id', participantId);
+      });
       
-      var participantId = $('#tpl').find('#participant_id').val()
-      var comment = $('#tpl').find('#comment').val()
-      
-      formData.append('reliable', reliable);
-      formData.append('record_taken', date);
-      formData.append('submitted_at', submittedAt);
-      formData.append('type_name', typeName);
-      formData.append('comment', comment);
-      formData.append('participant_id', participantId);
-    });
-    
-    this.on("successmultiple", function(data,response) {
-      $('#tpl').toggleClass('hidden')
-      $('#response').toggleClass('hidden')
-      
-      var textnode = document.createTextNode(response);
-      var div = document.getElementById('response')
-      div.innerHTML += response.toString()
-    });
-  }
-});
+      this.on("successmultiple", function(data,response) {
+        $('#tpl').toggleClass('hidden')
+        $('#response').toggleClass('hidden')
+        
+        var textnode = document.createTextNode(response);
+        var div = document.getElementById('response')
+        div.innerHTML += response.toString()
+      });
+    }
+  });
+}
 
