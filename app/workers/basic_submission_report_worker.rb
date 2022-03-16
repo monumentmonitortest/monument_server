fece8986582aa75a4989faee30567833e401081b
Rosie-Brigham <rosiebrigham@gmail.com>
2022-03-16 18:06:30 +0000

    updates submission report worker working with azure client

--- app/workers/basic_submission_report_worker.rb
+++ app/workers/basic_submission_report_worker.rb
@@ -5,36 +5,32 @@ class BasicSubmissionReportWorker
   def perform(email_address)
     @report = BasicSubmissionReportJob.new.perform
 
-    @local_tmp_directory = "tmp/basic_submissions"
-    @s3_directory = "archive/basic_submissions.csv"
+    @local_tmp_directory = "tmp/basic_submissions.csv"
+    @azure_directory = "archive/basic_submissions.csv"
 
    
-    s3Url = upload_to_s3(@local_tmp_directory, @s3_directory)
-    ZipMailer.job_done(email: email_address, url: s3Url).deliver_now
+    azureUrl = upload_to_azure(@local_tmp_directory, @azure_directory)
+    ZipMailer.job_done(email: email_address, url: azureUrl).deliver_now
   end
 
   private
 
-  def tmp_directory(directory_suffix)
-    
-  end
-
-  def s3_directory(directory_suffix)
-    "archive/basic_submissions"
-  end
-
-  def upload_to_s3(tmp_directory, s3_directory)
-    puts 'uploading to S3! (hopefully)'
+  def upload_to_azure(tmp_directory, azure_directory)
+    puts 'uploading to Azure! (hopefully)'
+   
     begin
-      s3 = Aws::S3::Resource.new(region:'eu-west-2')
-      if Rails.env == 'production'
-        obj = s3.bucket(ENV['S3_BUCKET_PRODUCTION']).object(s3_directory)
-      else
-        obj = s3.bucket(ENV['S3_BUCKET_DEVELOPMENT']).object(s3_directory)
-      end
-      obj.upload_file("#{tmp_directory}.csv")
-      obj.public_url
-    rescue Aws::S3::Errors::ServiceError
+      client = Azure::Storage::Blob::BlobService.create(storage_account_name: ENV['STORAGE_ACCOUNT_NAME'], storage_access_key:ENV['STORAGE_ACCESS_KEY'])
+      # if Rails.env == 'production'
+      #   obj = client.get_blob(ENV['STORAGE_CONTAINER'], azure_directory)
+      # else
+      #   # update this with devlopment storage container
+      #   obj = client.get_blob(ENV['STORAGE_CONTAINER'], azure_directory)
+      # end
+    
+      content = ::File.open(tmp_directory, 'rb') { |file| file.read }
+      client.create_block_blob(ENV['STORAGE_CONTAINER'], azure_directory, content)
+      url = "https://" + ENV['STORAGE_ACCOUNT_NAME'] + ".blob.core.windows.net/" + ENV['STORAGE_CONTAINER'] + "/#{azure_directory}"
+    rescue Azure::Core::Http::HTTPError #may need to be a different one
       puts 'BUGGER'
     end
   end  
