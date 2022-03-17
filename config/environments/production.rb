927a28dd6dd7df42008fcf860283267fe658a150
Rosie-Brigham <rosiebrigham@gmail.com>
2022-03-17 15:55:59 +0000

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'www.monumentmonitor.co.uk' }
+  config.action_mailer.default_url_options = { host: 'https://hes-mm-test.herokuapp.com/' }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
